9f3fd1d58bb44949ae7f795fab3273a5ab1278d7
Adiladil99 <adiladil9899@gmail.com>
2021-06-21 12:16:01 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar;
